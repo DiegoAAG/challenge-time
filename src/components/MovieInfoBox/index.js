87027dd6ee7d954b62921446f2b7e4912ab1f8a4
Diego <diegogoncalves@yebo.com.br>
2018-12-14 15:53:19 -0200

    some css work

--- src/components/MovieInfoBox/index.js
+++ src/components/MovieInfoBox/index.js
@@ -3,13 +3,49 @@ import { Visible } from 'react-grid-system';
 import propTypes from 'prop-types';
 import MoviePoster from '../MoviePoster'
 import styled from 'styled-components';
+import rottenEgg from './rotten-eg.png'
 
 const MovieBox = styled.div`
    color: white;
+align-content: center;
    font-family: Helvetica;
    p {
        font-style: italic;
    }
+`
+const SmallSticker = styled.img`
+align-content: center;
+padding-left: 25%;
+padding-top: 50%;
+max-width: 40px;
+max-height: 40px;
+
+`
+
+const MovieTab = styled.div`
+   display: -webkit-inline-box;
+   margin-top
+   color: white;
+   margin-top: 2%;
+   background-color: #403D3D;
+   border-color: gray;
+   border-width: 2px;
+   border-style: solid;
+   font-family: Helvetica;
+   text-align: left;
+   p {
+       font-style: italic;
+   }
+   h3 {
+       padding-left: 5%;
+       max-width: 25ch;
+       overflow: hidden;
+
+  }
+   height: 100%;
+   width: 100%;
+   border-radius: 5px;
+
 `
 const MovieInfoBox = ({ movieImage, movieTitle, movieYear, onPress }) => (
     <>
@@ -23,7 +59,9 @@ const MovieInfoBox = ({ movieImage, movieTitle, movieYear, onPress }) => (
 
         <Visible sm xs>
             <MovieBox onClick={onPress}>
-                <h3>{movieTitle} ({movieYear})</h3> 
+                <MovieTab>
+                 <SmallSticker src={rottenEgg}/><h3>{movieTitle} ({movieYear})</h3>
+                </MovieTab>
             </MovieBox>
         </Visible>
     </>
